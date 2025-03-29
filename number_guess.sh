a2573e92762e39ce936e51a4f2249e8a804c4e44
waynemareci <wayne@mareci.com>
2025-03-29 09:33:38 +0000

    feat: add new user

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname= --tuples-only -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 RAND=$(($RANDOM % 1000 + 1))
 
-echo "Random number: $RAND"
+echo "Enter your username:"
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
+
+if [[ -z $USER_ID ]]
+then
+  echo "didn't find username $USERNAME"
+  USER_ACCOUNT_CREATED=$($PSQL "INSERT INTO users (name) VALUES('$USERNAME')")
+  echo "USER_ACCOUNT_CREATED = $USER_ACCOUNT_CREATED"
+else
+  echo "found username $USERNAME"
+fi
 
 exit
